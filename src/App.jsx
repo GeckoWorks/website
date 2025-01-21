8a9975e955caf86df19af6542c9873f01d2051df
Gecko Works <geckoworks.work@gmail.com>
2025-01-21 20:26:03 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,6 @@ const App = () =>{
           <Navbar />
           <Hero />
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
       </div>
     </BrowserRouter>
   )
