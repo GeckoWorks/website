a76c665c23ff88b846d03e375fba0298f3357823
Apple M1 <applem1@Apples-MacBook-Air.local>
2025-01-21 20:16:54 +0545

    first commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,11 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+const Computers = () => {
+  return (
+    <div>Shrestha Nischal</div>
+  )
+}
+
+export default Computers
