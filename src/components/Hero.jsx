5f49c845c97ffee3f5ec43ec781f65b95c6f74a6
Apple M1 <applem1@Apples-MacBook-Air.local>
2025-01-27 09:27:36 +0545

    changed hero color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-screen mx-auto bg-[#FFFDD0]`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-[#101110]`}>
+          <h1 className={`${styles.heroHeadText} text-[#000000]`}>
             Hi, Welcome to Our New Website !! <span className='text-[#519b8b]'>Gecko Works</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-black-150`}>
